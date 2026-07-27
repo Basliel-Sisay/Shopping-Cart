b701c5d1b830d372e34de637269e04a69b851b16
Basliel-Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-07-27 21:15:11 +0300

    Update Navbar component with cart badge links

--- Shopping Cart/src/components/Navbar/Navbar.jsx
+++ Shopping Cart/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import{Link} from 'react-router-dom';
-function Navbar({totalItems}){
-    return(
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/shop'>Shop</Link>
-            <Link to='/cart'>Cart({totalItems})</Link>
-        </nav>
-    )
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+function Navbar({ totalItems }) {
+  return (
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.navLink}>
+        Home
+      </Link>
+      <Link to="/shop" className={styles.navLink}>
+        Shop
+      </Link>
+      <Link to="/cart" className={styles.cartLink}>
+        Cart ({totalItems})
+      </Link>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
