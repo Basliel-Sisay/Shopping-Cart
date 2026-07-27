3fa4ea115dd7db5a8651f81b6df7c9b7979a8947
Basliel-Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-07-27 21:15:09 +0300

    Refine Layout component with shared navigation structure

--- Shopping Cart/src/components/Navbar/Layout.jsx
+++ Shopping Cart/src/components/Navbar/Layout.jsx
@@ -1,9 +1,15 @@
-function Layout({getTotalCartItems}){
-    return (
-      <div>
-        <Navbar totalItems={getTotalCartItems()} />
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import styles from "./Layout.module.css";
+
+function Layout({ getTotalCartItems }) {
+  return (
+    <div className={styles.layout}>
+      <Navbar totalItems={getTotalCartItems()} />
+      <main className={styles.main}>
         <Outlet />
-      </div>
-    );
+      </main>
+    </div>
+  );
 }
-export default Layout
+export default Layout;
