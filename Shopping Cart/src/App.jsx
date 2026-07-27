88e24b8091b471759803fc04d76f44c844dfbc8d
Basliel-Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-07-27 21:02:25 +0300

    Add toast notifications for cart add and remove actions

--- Shopping Cart/src/App.jsx
+++ Shopping Cart/src/App.jsx
@@ -1,65 +1,91 @@
-import {Routes , Route} from 'react-router-dom';
-import { useState } from 'react';
-import Layout from './components/Navbar/Layout';
-import Home from './pages/Home/Home';
-import Shop from './pages/Shop/Shop';
-import Cart from './pages/Cart/Cart';
-
-function App(){
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Layout from "./components/Navbar/Layout";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import Cart from "./pages/Cart/Cart";
+import toast from "react-hot-toast";
+function App() {
   const [cart, setCart] = useState([]);
   function addToCart(product, selectedQuantity){
-    setCart(oldCart=>{
-      const added = oldCart.findIndex(exist => exist.id === product.id);
-    if(added !== -1){
-      const newCart = [...oldCart];
-      newCart[added]={
+    setCart((oldCart) => {
+      const added = oldCart.findIndex((exist) => exist.id === product.id);
+      let message = "";
+      if (added !== -1) {
+        const newCart = [...oldCart];
+        newCart[added] = {
           ...newCart[added],
-          quantity: newCart[added].quantity + selectedQuantity
-      };
-      return newCart;
-  }
-      return [...oldCart,{
+          amount: newCart[added].amount + selectedQuantity,
+        };
+        message = product.title + " x " + newCart[added].amount;
+        toast.success(message, {
+          duration: 2200,
+          style: {
+            borderRadius: '10px',
+            background: 'rgb(50, 53, 51)',
+            color: 'white',
+          },
+        });
+        return newCart;
+      }
+      message = product.title + " added to cart";
+      toast.success(message, {
+        duration: 2200,
+        style: {
+          borderRadius: '10px',
+          background: 'rgb(50, 53, 51)',
+          color: 'white',
+        },
+      });
+      return [
+        ...oldCart,
+        {
           ...product,
-          quantity: selectedQuantity
-      }];
-    }
-  );
-  };
-  function updateCartQuantity(productId, newQuantity){
-    setCart(oldCart=>{
-      if(newQuantity <=0){
-        const noNewProduct = oldCart.filter(item=> item.id !== productId);
-        return noNewProduct;
-    }
-    return oldCart.map(item=> item.id=== productId?{
-      ...item,
-      quantity: newQuantity
-    }:
-      item);
-
+          amount: selectedQuantity,
+        },
+      ];
     });
-  };
-
-  const removeFromCart = (productId) => {
-    setCart(oldCart => oldCart.filter(item => item.id !== productId));
-  };
-
+  }
+  function updateCartQuantity(productId, newQuantity){
+    setCart((oldCart) => {
+      if (newQuantity <= 0) {
+        return oldCart.filter((item) => item.id !== productId);
+      }
+      return oldCart.map((item) =>
+        item.id === productId ? { ...item, amount: newQuantity } : item
+      );
+    });
+  }
+  function removeFromCart(productId){
+    setCart((oldCart) => {
+      const item = oldCart.find((i) => i.id === productId);
+      if (item) {
+        toast.success(item.title + " removed from cart", {
+          duration: 2200,
+          style: {
+            borderRadius: '10px',
+            background: 'rgb(50, 53, 51)',
+            color: 'white',
+          },
+        });
+      }
+      return oldCart.filter((i) => i.id !== productId);
+    });
+  }
   function getTotalCartItems(){
-    let total =0;
-    cart.forEach(item=> {total= item.quantity + total});
+    let total = 0;
+    cart.forEach((item) => {
+      total = item.amount + total;
+    });
     return total;
-   }
-    
-
-  
-  return(
+  }
+  return (
     <Routes>
-  <Route element={<Layout getTotalCartItems={getTotalCartItems} />}>
-    <Route path="/" element={<Home />} />
-    <Route path="/shop" element={<Shop addToCart={addToCart} />} />
-    <Route path="/cart" element={<Cart cart={cart} updateCartQuantity={updateCartQuantity} removeFromCart={removeFromCart}/>}/></Route>
-</Routes>
+      <Route element={<Layout getTotalCartItems={getTotalCartItems} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop addToCart={addToCart} />} />
+        <Route path="/cart" element={ <Cart cart={cart} updateCartAmount={updateCartQuantity} removeFromCart={removeFromCart} />}/></Route>
+    </Routes>
   );
 }
-
 export default App;
