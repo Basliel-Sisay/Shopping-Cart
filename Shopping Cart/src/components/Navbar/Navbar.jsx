d00cd9f3a14a4c953f4a259865c1fa38733b6ca9
Basliel-Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-07-28 10:14:44 +0300

    Refactored the navigation component to dynamically apply an active class based on the current route

--- Shopping Cart/src/components/Navbar/Navbar.jsx
+++ Shopping Cart/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar({ totalItems }) {
+  const { pathname } = useLocation();
+
+  const linkClass = (path) =>
+    `${styles.navLink}${pathname === path ? ` ${styles.active}` : ""}`;
+
   return (
     <nav className={styles.nav}>
-      <Link to="/" className={styles.navLink}>
+      <Link to="/" className={linkClass("/")}>
         Home
       </Link>
-      <Link to="/shop" className={styles.navLink}>
+      <Link to="/shop" className={linkClass("/shop")}>
         Shop
       </Link>
-      <Link to="/cart" className={styles.cartLink}>
+      <Link to="/cart" className={linkClass("/cart")}>
         Cart ({totalItems})
       </Link>
     </nav>
